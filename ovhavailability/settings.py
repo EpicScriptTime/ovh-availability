f73d1b58a16d28c1e69dbe4d4d5a795b34dc27bb
DjDCH <coding@djdch.com>
2015-06-14 23:26:06 -0400

    Refactored project structure.

---
+++ ovhavailability/settings.py
@@ -0,0 +1,43 @@
+# Add your Twilio credentials for SMS notification
+TWILIO = {
+    'ACCOUNT_SID': '',
+    'AUTH_TOKEN': '',
+    'FROM': '',
+    'TO': '',
+}
+
+# Uncomment the DCs you want to watch
+WATCHED_DC_LIST = [
+    #'gra',
+    #'rbx-hz',
+    #'sbg',
+    #'rbx',
+    #'bhs',
+]
+
+# Uncomment the server you want to watch
+WATCHED_SERVER_LIST = [
+    #'KS-1',
+    #'KS-2',
+    #'KS-3',
+    #'KS-4',
+    #'KS-5A',
+    #'KS-5B',
+    #'SYS-IP-1',
+    #'E3-SAT-1',
+    #'E3-SSD-1',
+    #'SYS-IP-2',
+    #'E3-SAT-2',
+    #'E3-SSD-2',
+    #'E3-SAT-3',
+    #'E3-SSD-3',
+    #'SYS-IP-4',
+    #'E3-SAT-4',
+    #'E3-SSD-4',
+    #'BK-8T',
+    #'SYS-IP-5',
+    #'SYS-IP-5S',
+    #'BK-24T',
+    #'SYS-IP-6',
+    #'SYS-IP-6S',
+]
---
+++ ovhavailability/utils.py
@@ -0,0 +1,46 @@
+import os.path
+import pickle
+
+from twilio import TwilioRestException
+from twilio.rest import TwilioRestClient
+from collections import defaultdict
+
+import settings
+
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+
+SAVE_FILENAME = PATH + '/state.pickle'
+
+
+def multilevel_dict():
+    return defaultdict(multilevel_dict)
+
+
+def load_state(filename=SAVE_FILENAME):
+    state = {}
+
+    if os.path.isfile(filename):
+        stream = open(filename, 'rb')
+        state = pickle.load(stream)
+        stream.close()
+
+        if not isinstance(state, dict):
+            state = {}
+
+    return state
+
+
+def save_state(state, filename=SAVE_FILENAME):
+    stream = open(filename, 'wb')
+    pickle.dump(state, stream)
+    stream.close()
+
+
+def send_sms(body):
+    try:
+        client = TwilioRestClient(settings.TWILIO['ACCOUNT_SID'], settings.TWILIO['AUTH_TOKEN'])
+        message = client.messages.create(body=body, to=settings.TWILIO['TO'], from_=settings.TWILIO['FROM'])
+        return message.sid
+    except TwilioRestException as e:
+        raise e
